3dea6e2b23d579022002f94b9c6b4f84f281d8c2
Andre Farias <andre.lmfarias@gmail.com>
2018-09-29 21:23:48 +0200

    Did function occurences

--- cc1.py
+++ cc1.py
@@ -30,7 +30,13 @@ def last2(string):
 # Test it with the Lorem upsuj
 #"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 def occurences(text):
-  return
+    count = {}
+    for letter in text:
+        if letter in text:
+            count[letter] += 1
+        else:
+            count[letter] = 1
+    return count
 
 #Write a program that maps a list of words into a list of
 #integers representing the lengths of the correponding words.
