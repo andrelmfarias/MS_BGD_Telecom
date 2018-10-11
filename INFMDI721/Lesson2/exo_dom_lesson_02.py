683460abc06ca56979162de6454524f286238717
Andre Farias <andre.lmfarias@gmail.com>
2018-10-11 18:17:36 +0200

    Added get_pct_change function

--- INFMDI721/Lesson2/exo_dom_lesson_02.py
+++ INFMDI721/Lesson2/exo_dom_lesson_02.py
@@ -6,11 +6,6 @@ from bs4 import BeautifulSoup
 website_prefix = "https://www.reuters.com/finance/stocks/financial-highlights/"
 companies_url = {"LVMH":"LVMH.PA","Airbus":"AIR.PA","Danone":"DANO.PA"}
 
-def string_to_int_SP(string_SP):
-
-
-
-
 def get_soup(url):
     res = requests.get(url)
     if res.status_code == 200:
@@ -23,4 +18,11 @@ def get_share_price(company):
     soup = get_soup(url)
     specific_style = "font-size: 23px;"
     share_price = soup.find("span", style = specific_style).text
-    return float(share_price[5:])
+    return float(share_price.strip())
+
+def get_pct_change(company):
+    url = website_prefix + companies_url[company]
+    soup = get_soup(url)
+    specific_class = "valueContentPercent"
+    pct_change = soup.find("span", class_ = specific_class).text
+    return float(pct_change.strip()[1:-2]) # returns float x for a pct x% 
