9a774b0de7f2aed61c11fa36ade89e18525e545a
Andre Farias <andre.lmfarias@gmail.com>
2018-10-12 20:39:58 +0200

    Function get_disctount_avg done

--- INFMDI721/Lesson2/exo_cc_lesson_02.py
+++ INFMDI721/Lesson2/exo_cc_lesson_02.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 urls = {"acer": "https://www.fnac.com/SearchResult/ResultList.aspx?ItemPerPage=100&SCat=8!1%2c8002!2&Search=ordinateur+portable+acer&SFilt=55661!23&sft=1&sl",
-       "dell": "https://www.fnac.com/SearchResult/ResultList.aspx?SCat=8!1%2c8002!2&Search=ordinateur+portable+dell&SFilt=36064!23&sft=1&sl"}
+       "HP": "https://www.fnac.com/SearchResult/ResultList.aspx?SCat=8!1%2c8002!2&Search=ordinateur+portable+hp&SFilt=57046!23&sft=1&sl"}
 
 def get_soup(url):
     res = requests.get(url)
@@ -12,6 +12,13 @@ def get_soup(url):
         soup = BeautifulSoup(html_code,"html.parser")
     return soup
 
-def get_discount_list(brand):
+def get_discount_avg(brand):
     url = urls[brand]
     soup = get_soup(url)
+    html_code = soup.findAll("div", class_ = "red")
+    discount_list = []
+    for item in html_code:
+        html_text = item.text
+        discount = float(html_text[11:-2])
+        discount_list.append(discount)
+    return sum(discount_list)/len(discount_list)
