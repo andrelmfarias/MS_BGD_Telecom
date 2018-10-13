fec9b1abf74f59fb7669a4fa3cad40f82cd48574
Andre Farias <andre.lmfarias@gmail.com>
2018-10-13 15:30:25 +0200

    Finalized course exercise for HP and Acer brands

--- INFMDI721/Lesson2/exo_cc_lesson_02.py
+++ INFMDI721/Lesson2/exo_cc_lesson_02.py
@@ -2,8 +2,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-urls = {"acer": "https://www.fnac.com/SearchResult/ResultList.aspx?ItemPerPage=100&SCat=8!1%2c8002!2&Search=ordinateur+portable+acer&SFilt=55661!23&sft=1&sl",
-       "HP": "https://www.fnac.com/SearchResult/ResultList.aspx?SCat=8!1%2c8002!2&Search=ordinateur+portable+hp&SFilt=57046!23&sft=1&sl"}
+urls = {"acer": "https://www.fnac.com/SearchResult/ResultList.aspx?PageIndex={page}&SCat=8!1%2c8002!2&Search=ordinateur+portable+acer&SFilt=55661!23&sft=1&sl",
+       "HP": "https://www.fnac.com/SearchResult/ResultList.aspx?PageIndex={page}&SCat=8!1%2c8002!2&Search=ordinateur+portable+hp&SFilt=57046!23&sft=1&sl"}
+
+n_pages = {"acer": 5, "HP": 16}
 
 def get_soup(url):
     res = requests.get(url)
@@ -14,11 +16,18 @@ def get_soup(url):
 
 def get_discount_avg(brand):
     url = urls[brand]
-    soup = get_soup(url)
-    html_code = soup.findAll("div", class_ = "red")
     discount_list = []
-    for item in html_code:
-        html_text = item.text
-        discount = float(html_text[11:-2])
-        discount_list.append(discount)
-    return sum(discount_list)/len(discount_list)
+    for page in range(1,n_pages[brand]+1):
+        soup = get_soup(url.format(page=page))
+        html_code = soup.findAll("div", class_ = "red")
+        for item in html_code:
+            html_text = item.text
+            discount = float(html_text[11:-2])
+            discount_list.append(discount)
+    if discount_list: return sum(discount_list)/len(discount_list)
+    else: return 0
+
+HP_discount_avg = round(get_discount_avg("HP"),2)
+Acer_discount_avg = round(get_discount_avg("acer"),2)
+print(f"HP average discount: {HP_discount_avg}%")
+print(f"Acer average discount: {Acer_discount_avg}%")
