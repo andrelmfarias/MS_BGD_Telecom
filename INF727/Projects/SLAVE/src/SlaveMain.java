3a1503d89d0e57e3afd1fe73feca0b89b57f8420
Andre Farias <andre.lmfarias@gmail.com>
2018-10-15 10:00:23 +0200

    Q49 of INF727 done (Shuffling)

--- INF727/Projects/SLAVE/src/SlaveMain.java
+++ INF727/Projects/SLAVE/src/SlaveMain.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -11,26 +12,32 @@ public class SlaveMain {
 	public static void main(String[] args) throws InterruptedException, IOException {
 		
 		String mode = args[0];
-		String input_path = args[1]; // path in the form: /tmp/amacedo/split/S*.txt
-		
-		//System.out.println("mode: "+mode);
-		//System.out.println("input path: "+input_path);
-		
-		String[] ipath = input_path.split("/");
-		String ifile_name = ipath[ipath.length-1];
-		String txt_number = ifile_name.substring(1, ifile_name.length()-4);
-		
-		String output_dir = "/tmp/amacedo/maps";
-	
-		String output_path = output_dir + "/UM" + txt_number + ".txt";
-		
-		//System.out.println("output path: "+output_path);
-		//System.out.println(mode.equals("0"));
 
 		if (mode.equals("0")) {
+			String input_path = args[1]; // path in the form: /tmp/amacedo/split/S*.txt
+			
+			// Getting number of file
+			String[] ipath = input_path.split("/");
+			String ifile_name = ipath[ipath.length-1]; 
+			String txt_number = ifile_name.substring(1, ifile_name.length()-4);
+			
+			String output_dir = "/tmp/amacedo/maps";
+			String output_path = output_dir + "/UM" + txt_number + ".txt";
+			
 			makeDir(output_dir);
 			map(input_path,output_path);
 		}	
+		
+		if(mode.equals("1")) {
+			String key = args[1];
+			String sm_path = args[2];
+			ArrayList<String> um_files = new ArrayList<String>();
+			for(int i = 3; i < args.length; i++) {
+				um_files.add(args[i]);
+			}
+			shuffle(key, sm_path, um_files);
+			
+		}
 	}
 
 	public static void map(String ipath, String opath) throws IOException{
@@ -65,5 +72,25 @@ public class SlaveMain {
 		p.waitFor(3,TimeUnit.SECONDS);
 
 	}
+	
+	public static void shuffle(String key, String sm_path, ArrayList<String> um_files) throws IOException {
+		FileWriter writer;
+		writer = new FileWriter(sm_path);
+		for (String um_path: um_files) {
+			FileReader in = new FileReader(um_path);
+			BufferedReader br = new BufferedReader(in);
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				if(key.equals(line.split(" ")[0])) {
+					line = key + " 1" + "\n";
+					writer.write(line);
+				}
+			}
+			in.close();
+			br.close();
+		}
+		writer.close();		
+	}
+	
 	
 }
--- INF727/Projects/Test/src/Test.java
+++ INF727/Projects/Test/src/Test.java
@@ -5,28 +5,10 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		HashMap<String,ArrayList<String>> dict = new HashMap<String,ArrayList<String>>();
-		insertElement(dict,"car","UM0.txt");
-		
-		insertElement(dict,"car","UM1.txt");
-		
-		insertElement(dict,"beer","UM1.txt");
-		
-		System.out.println(dict);
+		String abc = "asai nsiajsi";
+		System.out.println(abc.split(" ")[0]);
 		
 
-	}
-	
-	public static void insertElement(HashMap<String,ArrayList<String>> dict, String word_key, String um_map) {
-		if(dict.containsKey(word_key)) {
-			ArrayList<String> umList = dict.get(word_key);
-			umList.add(um_map);
-			dict.replace(word_key, umList);
-		}else {
-			ArrayList<String> umList = new ArrayList<String>();
-			umList.add(um_map);
-			dict.put(word_key, umList);
-		}
 	}
 
 }
