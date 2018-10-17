f19a1347b2b29eb8bc5f0381351693fcd18e3df1
Andre Farias <andre.lmfarias@gmail.com>
2018-10-17 13:40:52 +0200

    Exo3 Kit-DataScience: function get_json done

--- INFMDI721_KitDS/Lesson2/exo_dom_lesson_02.py
+++ INFMDI721_KitDS/Lesson2/exo_dom_lesson_02.py
@@ -85,7 +85,7 @@ financials_df = pd.DataFrame(all_financials).transpose()
 # Changing columns to a proper order
 columnsTitles = ["SP (€)","% change","Q4 sales (m€)",  \
               "% of shares owned by inst. inv.",       \
-              "DY Company(%)","DY Industry(%)","DY Sector(%)"]
+              "DY Company(%)", "DY Industry(%)", "DY Sector(%)"]
 financials_df = financials_df.reindex(columns=columnsTitles)
 
 print(financials_df)
--- INFMDI721_KitDS/Lesson3/exo_dom_lesson_03.py
+++ INFMDI721_KitDS/Lesson3/exo_dom_lesson_03.py
@@ -3,13 +3,21 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://gist.github.com/paulmillr/2657075"
+url_API = "https://api.github.com/users/{user}/repos"
+
+# reading token from .txt file
+with open('token.txt') as f:
+    content = f.readlines()
+git_token = content[0].strip()
 
 def get_soup(url):
     res = requests.get(url)
     if res.status_code == 200:
         html_code = res.text
         soup = BeautifulSoup(html_code,"html.parser")
-    return soup
+        return soup
+    else:
+        return None
 
 def get_list_of_users(url):
     lst_users = []
@@ -22,3 +30,11 @@ def get_list_of_users(url):
     return lst_users
 
 users = get_list_of_users(url)
+
+def get_json(user):
+    url_user = url_API.format(user=user)
+    res = requests.get(url_user, headers={"Authorization": git_token})
+    if res.status_code == 200:
+        return res.json()[0] # return json element
+    else:
+        return None
