65be203c3b38332e3766dd58cbccc3d6882ad481
Andre Farias <andre.lmfarias@gmail.com>
2018-10-18 18:05:47 +0200

    Implemeted parallelilization -- time reached : 5min

--- INFMDI721_KitDS/Lesson3/exo_dom_lesson_03.py
+++ INFMDI721_KitDS/Lesson3/exo_dom_lesson_03.py
@@ -1,8 +1,10 @@
 # coding: utf-8
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+import multiprocessing as mp
 import time
+
 
 start_time = time.time()
 
@@ -18,7 +20,7 @@ def get_soup(url):
     res = requests.get(url)
     if res.status_code == 200:
         html_code = res.text
-        soup = BeautifulSoup(html_code,"html.parser")
+        soup = BeautifulSoup(html_code, "html.parser")
         return soup
     else:
         print("Error: HTML code not available")
@@ -30,7 +32,7 @@ def get_list_of_users(url):
     soup = get_soup(url)
     if soup is not None:
         lst_html = soup.find_all("tr")
-        for i in range(1,256+1):
+        for i in range(1, 256+1):
             row = lst_html[i].text
             user = row.split(' ')[2]
             lst_users.append(user)
@@ -52,7 +54,7 @@ def get_json_list(user):
                 stop = True
             page = page + 1
         else:
-            print("Error: json list not available for user:" + user)
+            print("Error: json list not available for user: " + user)
             print("Status code: " + res.status_code)
     return json_list
 
@@ -62,11 +64,7 @@ def get_tot_stars(json_list):
         stars = stars + json_element['stargazers_count']
     return stars
 
-
-users = get_list_of_users(url)
-
-users_dict = {}
-for user in users:
+def insert_info_in_dict(user,user_dict):
     json_list = get_json_list(user)
     n_repos = len(json_list)
     stars = get_tot_stars(json_list)
@@ -78,13 +76,33 @@ for user in users:
                         '#_repos' : n_repos,
                         'mean_rating' : stars_mean}
 
+users = get_list_of_users(url)
+
+jobs = []
+users_dict = {}
+pr_tracker = 0 # tracker to avoid launching all processes at once
+for user in users:
+    p = mp.Process(target=insert_info_in_dict,args=(user,users_dict))
+    jobs.append(p)
+    p.start()
+    pr_tracker = (pr_tracker+1) % 5 # for each group of 5 processes we wait
+    if pr_tracker == 0:
+        for prc in jobs:
+            prc.join()
+        print("Tracker reinitiated at user: " + user + " --> at index " + str(users.index(user)))
+        jobs = [] # reinitiate jobs list
+
+# applying join to the remaining process in job list
+for p in jobs:
+    p.join()
+
 df = pd.DataFrame.from_dict(users_dict, orient='index',
                             columns=['tot_stars','#_repos','mean_rating'])
 df.reset_index(level=0, inplace=True)
 df.rename(columns={'index': 'user'}, inplace=True)
 
 sorted_df = df.sort_values(by='mean_rating', ascending=False)
+print(sorted_df)
 
 execution_time = (time.time() - start_time) # time in seconds
-print(sorted_df)
-print("Total time: " + str(execution_time/60) + "min")
+print("Total time: " + str(round(execution_time/60),1) + "min")
