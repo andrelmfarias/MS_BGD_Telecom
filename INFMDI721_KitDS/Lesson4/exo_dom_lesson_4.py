4054ae7547cb55a1ae0519b67e1adcebac57eb10
Andre Farias <andre.lmfarias@gmail.com>
2018-10-26 10:06:00 +0200

    INFMDI721 -- can get all links to ads

--- INFMDI721_KitDS/Lesson4/exo_dom_lesson_4.py
+++ INFMDI721_KitDS/Lesson4/exo_dom_lesson_4.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import numpy as np
 import requests
 from bs4 import BeautifulSoup
+import multiprocessing as mp
 
 url = "https://www.lacentrale.fr/listing?makesModelsCommercialNames=RENAULT%3AZOE&options=&page={page}&regions={region}"
 regions = ['FR-IDF', 'FR-PAC', 'FR-NAQ']
+ann_prefix = 'https://www.lacentrale.fr/'
 
 
 def get_soup(url):
@@ -29,16 +32,38 @@ def get_url_list(url):
     return url_list
 
 
+def append_urls_to_list(urls_list, page, region):
+    urls_list.append(get_url_list(url.format(page=page, region=region)))
+
+
+def get_tot_num_pages(url):
+    soup = get_soup(url)
+    html_code = soup.find("span", class_="numAnn")
+    num_ann = int(html_code.text)
+    pages = np.ceil(num_ann/16)  # there are 16 announces per page
+    return int(pages)
+
+
+# Getting number of pages for each for each region
+dict_pages = {}
+for region in regions:
+    num_pages = get_tot_num_pages(url.format(page=1, region=region))
+    dict_pages[region] = num_pages
+
+# Getting url links to all annonces
 urls_dict = {}
 for region in regions:
-    page = 1
-    url_list_per_region = []
-    while True:
-        url_list = get_url_list(url.format(page=page, region=region))
-        if len(url_list) == 0:
-            break
-        url_list_per_region += url_list
-        page += 1
-    urls_dict[region] = url_list_per_region
+    # preparing for process parallelization
+    jobs = []
+    manager = mp.Manager()
+    urls_list = manager.list()
+    for page in range(1, dict_pages[region]+1):
+        p = mp.Process(target=append_urls_to_list, args=(urls_list, page, region))
+        jobs.append(p)
+        p.start()
+    # waiting for all requests to finish
+    for p in jobs:
+        p.join()
+    urls_dict[region] = list(urls_list)
 
 print(urls_dict)
