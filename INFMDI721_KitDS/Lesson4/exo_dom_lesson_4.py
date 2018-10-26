dc3ae4f683958f1cab1a1ec41dfbe7641c9b62a5
Andre Farias <andre.lmfarias@gmail.com>
2018-10-26 13:08:37 +0200

    KIT-DS: implemented function that get all info into a dict, parallelized in order to work faster

--- INFMDI721_KitDS/Lesson4/exo_dom_lesson_4.py
+++ INFMDI721_KitDS/Lesson4/exo_dom_lesson_4.py
@@ -3,10 +3,11 @@ import numpy as np
 import requests
 from bs4 import BeautifulSoup
 import multiprocessing as mp
+import re
 
 url = "https://www.lacentrale.fr/listing?makesModelsCommercialNames=RENAULT%3AZOE&options=&page={page}&regions={region}"
 regions = ['FR-IDF', 'FR-PAC', 'FR-NAQ']
-ann_prefix = 'https://www.lacentrale.fr/'
+ad_prefix = 'https://www.lacentrale.fr'
 
 
 def get_soup(url):
@@ -33,15 +34,65 @@ def get_url_list(url):
 
 
 def append_urls_to_list(urls_list, page, region):
-    urls_list.append(get_url_list(url.format(page=page, region=region)))
+    # concatenation list1 + list2 does not work with mp.list types
+    tmp_list = (get_url_list(url.format(page=page, region=region)))
+    for element in tmp_list:
+        urls_list.append(element)
 
 
 def get_tot_num_pages(url):
     soup = get_soup(url)
     html_code = soup.find("span", class_="numAnn")
-    num_ann = int(html_code.text)
-    pages = np.ceil(num_ann/16)  # there are 16 announces per page
+    num_ad = int(html_code.text)
+    pages = np.ceil(num_ad/16)  # there are 16 ads per page
     return int(pages)
+
+
+def get_version(soup):
+    html_code = soup.find("div", class_="versionTxt txtGrey7C sizeC mB10 hiddenPhone")
+    version = html_code.text.strip()
+    return version
+
+
+def get_year(soup):
+    html_code = soup.find("span", class_="clearPhone lH35")
+    year = int(html_code.text)
+    return year
+
+
+def get_km(soup):
+    km_str = soup.find("span", class_="clearPhone lH40").text
+    km_lst = re.findall(r"(\d+)", km_str)
+    km = int("".join(km_lst))
+    return km
+
+
+def get_price(soup):
+    price_str = soup.find("strong", class_="sizeD lH35 inlineBlock vMiddle ").text.strip()
+    price_lst = re.findall(r"(\d+)", price_str)
+    price = int("".join(price_lst))
+    return price
+
+
+def get_number(soup):
+    number_str = soup.find("div", class_="phoneNumber1").text.strip()
+    number_lst = re.findall(r"(\d+)", number_str)
+    number = "".join(number_lst)
+    return number
+
+
+def get_info_per_ad(ad_link):
+    soup = get_soup(ad_prefix + ad_link)
+    version = get_version(soup)
+    year = get_year(soup)
+    km = get_km(soup)
+    price = get_price(soup)
+    number = get_number(soup)
+    return [version, year, km, price, number]
+
+
+def insert_info_in_dict(ads_dict, ad_link, region):
+    ads_dict[ad_link] = get_info_per_ad(ad_link) + [region]
 
 
 # Getting number of pages for each for each region
@@ -66,4 +117,21 @@ for region in regions:
         p.join()
     urls_dict[region] = list(urls_list)
 
-print(urls_dict)
+get_info_per_ad(urls_dict['FR-IDF'][0])
+
+# getting all information from ads into a dictionary, working parallelized
+manager = mp.Manager()
+ads_dict = manager.dict()
+jobs = []
+for region in regions:
+    for ad_link in urls_dict[region]:
+        p = mp.Process(target=insert_info_in_dict, args=(ads_dict, ad_link, region))
+        jobs.append(p)
+        p.start()
+
+for p in jobs:
+    p.join()
+
+ads_dict = dict(ads_dict)
+
+print(ads_dict)
