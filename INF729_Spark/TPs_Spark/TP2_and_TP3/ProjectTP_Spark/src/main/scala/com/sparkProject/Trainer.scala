d70d36ac668cb691d07f3f8439abe0b0b0e65708
Andre Farias <andre.lmfarias@gmail.com>
2018-10-31 09:06:29 +0100

    INF 729 Spark - TP3 Q3 done / started Q4

--- INF729_Spark/TPs_Spark/TP2_and_TP3/ProjectTP_Spark/src/main/scala/com/sparkProject/Trainer.scala
+++ INF729_Spark/TPs_Spark/TP2_and_TP3/ProjectTP_Spark/src/main/scala/com/sparkProject/Trainer.scala
@@ -2,6 +2,7 @@ package com.sparkProject
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.feature._
 
 
 object Trainer {
@@ -39,6 +40,67 @@ object Trainer {
       *       if problems with unimported modules => sbt plugins update
       *
       ********************************************************************************/
+
+    // 2.a. 1st stage: Building tokenizer
+
+    val tokenizer = new RegexTokenizer()
+      .setPattern("\\W+")
+      .setGaps(true)
+      .setInputCol("text")
+      .setOutputCol("tokens")
+
+
+    // 2.b. 2nd stage: Stop words
+
+    val remover = new StopWordsRemover()
+      .setInputCol(tokenizer.getOutputCol)
+      .setOutputCol("words_removed")
+
+    // 2.c. 3rd stage: CountVectorizer
+
+    val vectorizer = new CountVectorizer()
+      .setInputCol(remover.getOutputCol)
+      .setOutputCol("text_vectorized")
+    // .setMinTf()
+    // .setVocabSize()
+
+    // 2.d. 4th stage: IDF
+
+    val idf = new IDF()
+      .setInputCol(vectorizer.getOutputCol)
+      .setOutputCol("tfidf")
+    // .setMinDocFreq()
+
+    // 3.e. 5th stage: convert category variable country2 to indexes
+
+    val indexer_country = new StringIndexer()
+      .setInputCol("country2")
+      .setOutputCol("country_indexed")
+
+    // 3.f. 6th stage: convert category variable currency2 to indexes
+
+    val indexer_currency = new StringIndexer()
+      .setInputCol("currency2")
+      .setOutputCol("currency_indexed")
+
+    // 3.g. 6th stage: convert category variable country2 to to one-hot-encoder
+
+    val encoder_country = new OneHotEncoder()
+      .setInputCol(indexer_country.getOutputCol)
+      .setOutputCol("country_indexed")
+
+    // 3.g. 7th stage: convert category variable currency2 to to one-hot-encoder
+
+    val encoder_country = new OneHotEncoder()
+      .setInputCol(indexer_currency.getOutputCol)
+      .setOutputCol("currency_indexed")
+
+    // 4.h. 8th stage: assembly columns of features into one column to use ML algorithms
+
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("tfidf", "days_campaign", "hours_prepa", "goal", "country_indexed", "currency_indexed"))
+      .setOutputCol("features")
+
 
     println("hello world ! from Trainer")
 
